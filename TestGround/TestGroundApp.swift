32837523f483b70dc1281b5f46ce7a336196c7d1
Sam Nwangwu <samuel.nwangwu@outlook.com>
2024-07-20 12:03:34 +0100

    Initial Commit

---
+++ TestGround/TestGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestGroundApp.swift
+//  TestGround
+//
+//  Created by User on 20/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
